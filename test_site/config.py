9e428b12f44bf13faaf9b0b8945e55d34e63fb90
Juls <talaevaiulia@yandex.ru>
2024-10-30 09:50:14 +0300

    Изменения положения script cdn.jsdelivr внутри файлов base

--- test_site/config.py
+++ test_site/config.py
@@ -4,11 +4,11 @@ load_dotenv(dotenv_path='test_site/.env')
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
     MAIL_SERVER=os.getenv('MAIL_SERVER')
     MAIL_PORT=os.getenv('MAIL_PORT')
     MAIL_USERNAME=os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD=os.getenv('MAIL_PASSWORD')
-    MAIL_USE_SSL=True
+    MAIL_USE_SSL=os.getenv('MAIL_USE_SSL')
     
     
